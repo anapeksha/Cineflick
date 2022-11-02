ddcb03b2635723bba5fbb7b60319d522e0ef934d
anapeksha <anapeksha.mukherjee@gmail.com>
2022-11-02 16:48:04 +0530

    server side rendering enabled

--- src/pages/trending.tsx
+++ src/pages/trending.tsx
@@ -1,23 +1,19 @@
-import { useEffect, useState } from "react";
 import BasicGrid from "../components/BasicGrid";
 import { trendingMovies } from "../utils";
 
-const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-
-const Trending = () => {
-	const [trending, setTrending] = useState([]);
-
-	useEffect(() => {
-		trendingMovies(1, "day").then((data: any) => {
-			setTrending(data.results);
-		});
-	}, []);
-
+const Trending = (props) => {
 	return (
 		<div>
-			<BasicGrid data={trending} />
+			<BasicGrid data={props.data.results} />
 		</div>
 	);
 };
 
 export default Trending;
+
+export const getServerSideProps = async () => {
+	var data: any = await trendingMovies(1, "day");
+	return {
+		props: { data: data },
+	};
+};
