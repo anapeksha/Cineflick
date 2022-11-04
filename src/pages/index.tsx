32f464136264f78a559c8c01bfb6cf0452ae1dae
anapeksha <anapeksha.mukherjee@gmail.com>
2022-11-04 15:36:30 +0530

    built the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,80 @@
-import { Box, Typography } from "@mui/material";
-const Home = () => {
+import { Box, Typography, Grid, Divider } from "@mui/material";
+import { GetServerSideProps } from "next";
+import { getTopRated, handleImage, getUpcoming } from "../utils";
+import HomeCard from "../components/HomeCard";
+import { useRouter } from "next/router";
+
+const Home = (props: any) => {
+	const router = useRouter();
 	return (
-		<div>
-			<main>
-				<Box>
-					<Typography variant="h4" style={{ textAlign: "center" }}>
-						Welcome to Finderbar
-					</Typography>
-					<Typography variant="h6" style={{ textAlign: "center" }}>
-						Click here to continue
-					</Typography>
-				</Box>
-			</main>
-		</div>
+		<main>
+			<Box sx={{ p: 5 }}>
+				<Typography
+					variant="h3"
+					sx={{ textAlign: "center", fontFamily: "Monospace" }}
+				>
+					Top Rated
+				</Typography>
+				<Grid
+					sx={{ flexGrow: 1, padding: "20px", mb: 5 }}
+					container
+					spacing={3}
+					columns={6}
+				>
+					{props.topMoviesData.results.map((result: any, i: number) => {
+						if (i < 6) {
+							return (
+								<Grid item xs={3} sm={2} md={1} key={i}>
+									<HomeCard
+										image={handleImage(result.poster_path)}
+										altText={props.title}
+									/>
+								</Grid>
+							);
+						}
+					})}
+				</Grid>
+				<Divider variant="middle" flexItem sx={{}} />
+				<Typography
+					variant="h3"
+					sx={{ textAlign: "center", fontFamily: "Monospace", mt: 5 }}
+				>
+					Upcoming
+				</Typography>
+				<Grid
+					sx={{ flexGrow: 1, padding: "20px", mb: 5 }}
+					container
+					spacing={2.5}
+					columns={6}
+				>
+					{props.upcomingMoviesData.results.map((result: any, i: number) => {
+						if (i < 6) {
+							return (
+								<Grid item xs={3} sm={2} md={1} key={i}>
+									<HomeCard
+										image={handleImage(result.poster_path)}
+										altText={props.title}
+									/>
+								</Grid>
+							);
+						}
+					})}
+				</Grid>
+			</Box>
+		</main>
 	);
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	var { id } = context.query;
+	var topMoviesData: any = await getTopRated();
+	var upcomingMoviesData: any = await getUpcoming();
+	return {
+		props: {
+			topMoviesData: topMoviesData,
+			upcomingMoviesData: upcomingMoviesData,
+		},
+	};
+};
