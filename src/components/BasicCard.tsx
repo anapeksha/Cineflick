89d8f726c19742b8111b076fc3b4def6fbea72a8
anapeksha <anapeksha.mukherjee@gmail.com>
2022-11-04 15:30:40 +0530

    Next image used for cards

--- src/components/BasicCard.tsx
+++ src/components/BasicCard.tsx
@@ -1,26 +1,29 @@
-import { CardActionArea } from "@mui/material";
+import { CardActionArea, Box } from "@mui/material";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
 import ICardProps from "../interfaces/ICardProps";
-import AnimatedCard from "../styles/AnimatedCard.style";
+import AnimatedBasicCard from "../styles/AnimatedBasicCard.style";
+import Image from "next/image";
 
 const BasicCard: React.FC<ICardProps> = (props) => {
 	return (
-		<AnimatedCard
+		<AnimatedBasicCard
 			sx={{ maxWidth: 345 }}
 			elevation={6}
 			onClick={props.handleClick}
 		>
 			<CardActionArea>
-				<CardMedia
-					component="img"
-					height="140"
-					image={props.image}
-					alt={props.altText}
-					style={{ objectFit: "contain" }}
-				/>
+				<CardMedia style={{ display: "flex", justifyContent: "center" }}>
+					<Image
+						src={props.image}
+						width="250"
+						height="140"
+						style={{ objectFit: "contain" }}
+						alt={props.altText}
+					/>
+				</CardMedia>
 				<CardContent>
 					<Typography
 						gutterBottom
@@ -33,7 +36,7 @@ const BasicCard: React.FC<ICardProps> = (props) => {
 					</Typography>
 				</CardContent>
 			</CardActionArea>
-		</AnimatedCard>
+		</AnimatedBasicCard>
 	);
 };
 
