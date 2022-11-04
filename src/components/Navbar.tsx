6739a9e80aaeba26f05d5e2c98a722dde2a58aeb
anapeksha <anapeksha.mukherjee@gmail.com>
2022-11-04 15:35:34 +0530

    login links removed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,16 +15,6 @@ import ThemeSwitch from "../styles/ThemeSwitch.style";
 import { darkTheme, lightTheme } from "../utils/theme";
 
 const Navbar: React.FC<INavbarProps> = (props) => {
-	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-
-	const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-		setAnchorEl(event.currentTarget);
-	};
-
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
-
 	return (
 		<Box sx={{ flexGrow: 1 }}>
 			<AppBar position="static">
@@ -59,64 +49,6 @@ const Navbar: React.FC<INavbarProps> = (props) => {
 							}
 						}}
 					/>
-					{true && (
-						<div>
-							<IconButton
-								size="large"
-								aria-label="account of current user"
-								aria-controls="menu-appbar"
-								aria-haspopup="true"
-								onClick={handleMenu}
-								color="inherit"
-							>
-								<AccountCircle />
-							</IconButton>
-							<Menu
-								id="menu-appbar"
-								anchorEl={anchorEl}
-								PaperProps={{
-									elevation: 0,
-									sx: {
-										overflow: "visible",
-										filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-										mt: 1.5,
-										"& .MuiAvatar-root": {
-											width: 32,
-											height: 32,
-											ml: -0.5,
-											mr: 1,
-										},
-										"&:before": {
-											content: '""',
-											display: "block",
-											position: "absolute",
-											top: 0,
-											right: 14,
-											width: 10,
-											height: 10,
-											bgcolor: "background.paper",
-											transform: "translateY(-50%) rotate(45deg)",
-											zIndex: 0,
-										},
-									},
-								}}
-								anchorOrigin={{
-									vertical: "bottom",
-									horizontal: "right",
-								}}
-								keepMounted
-								transformOrigin={{
-									vertical: "top",
-									horizontal: "right",
-								}}
-								open={Boolean(anchorEl)}
-								onClose={handleClose}
-							>
-								<MenuItem onClick={handleClose}>Profile</MenuItem>
-								<MenuItem onClick={handleClose}>Wishlist</MenuItem>
-							</Menu>
-						</div>
-					)}
 				</Toolbar>
 			</AppBar>
 		</Box>
