c4e98668372157043577dbced1751b2eeffbfe4d
Anapeksha Mukherjee <anapeksha.mukherjee@gmail.com>
2022-11-27 11:36:55 +0530

    redundant logs removed

--- src/lib/auth/isAuthenticated.ts
+++ src/lib/auth/isAuthenticated.ts
@@ -3,12 +3,10 @@ import axios from "axios";
 const isAuthenticated = async () => {
 	try {
 		const res = await axios.get("/api/auth/getUser");
-		console.log(res);
 		if (res.status === 200 && res.data.user.type === "user") {
 			return true;
 		} else return false;
 	} catch (err) {
-		console.log(err);
 		return false;
 	}
 };
