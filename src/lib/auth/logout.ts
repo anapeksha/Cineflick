30591ce8e98100c3c6f5f5062cc907ea49c9a28e
Anapeksha Mukherjee <anapeksha.mukherjee@gmail.com>
2022-12-05 17:31:35 +0530

    redirect location changed

--- src/lib/auth/logout.ts
+++ src/lib/auth/logout.ts
@@ -4,7 +4,8 @@ const logout = async () => {
 	try {
 		const response = await axios.get("/api/auth/logout");
 		if (response.status === 200) {
-			window.location.href = "/";
+			window.location.href = "/login";
+			localStorage.removeItem("watchlist");
 			return true;
 		} else return false;
 	} catch (err: any) {
