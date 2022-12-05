cbeec6330e410df8364c215f8de80ea4a4f4464f
Anapeksha Mukherjee <anapeksha.mukherjee@gmail.com>
2022-12-05 17:31:09 +0530

    context changed

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Backdrop, CircularProgress, Box } from "@mui/material";
-import { useAuthenticationContext } from "../lib/context/authenticatedContext";
+import { useLoadingContext } from "../lib/context/loadedContext";
 
 const LoginLoader = () => {
-	const { isLoading } = useAuthenticationContext();
+	const { isLoading } = useLoadingContext();
 
 	return (
 		<Backdrop
 			sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
 			open={isLoading}
+			invisible={true}
 		>
 			<CircularProgress color="inherit" />
 		</Backdrop>
