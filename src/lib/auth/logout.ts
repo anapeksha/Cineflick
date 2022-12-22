f2ec2bc3dd87e441b37ee6e52c33e036f202692b
Anapeksha Mukherjee <anapeksha.mukherjee@gmail.com>
2022-12-22 15:52:57 +0530

    removed localstorage items first

--- src/lib/auth/logout.ts
+++ src/lib/auth/logout.ts
@@ -4,9 +4,9 @@ const logout = async () => {
 	try {
 		const response = await axios.get("/api/auth/logout");
 		if (response.status === 200) {
-			window.location.href = "/";
 			localStorage.removeItem("watchlist");
 			localStorage.removeItem("photo");
+			window.location.href = "/";
 			return true;
 		} else return false;
 	} catch (err: any) {
