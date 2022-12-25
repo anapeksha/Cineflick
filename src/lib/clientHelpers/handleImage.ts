0e6aa42aac393398102efb8c6dddc1f962c11d57
Anapeksha Mukherjee <anapeksha.mukherjee@gmail.com>
2022-12-25 18:18:13 +0530

    image quality modified for improved page load time

--- src/lib/clientHelpers/handleImage.ts
+++ src/lib/clientHelpers/handleImage.ts
@@ -1,6 +1,6 @@
 const handleImage: any = (image: string) => {
 	if (image !== null) {
-		return `https://image.tmdb.org/t/p/w500${image}`;
+		return `https://image.tmdb.org/t/p/w300${image}`;
 	} else return "/not-found.jpg";
 };
 
